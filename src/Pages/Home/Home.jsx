d712eb1889073750fe4a718b91534f468ffccc1b
osama355 <muhammadosama3556@gmail.com>
2021-11-02 17:48:40 +0500

    setting form

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./home.css";
+import {TextField, MenuItem} from '@mui/material'
+import Categories, {} from '../../Data/Categories'
 
 function Home() {
   return (
@@ -8,7 +10,23 @@ function Home() {
         <h3>Settings</h3>
       </div>
       <div className="main">
-        <div className="form">Form</div>
+        <div className="form">
+          <TextField className="textField1" style={{marginBottom:"30px"}} variant="outlined" label="Enter Name" />
+          <TextField variant="outlined" className="textField2" select label="Select category">
+            {
+              Categories.map((val,key)=>(
+                <MenuItem key={key}>
+                  {val.category}
+                </MenuItem>
+              ))
+            } 
+          </TextField>
+          <TextField select label="Select difficulty" variant="outlined" style={{marginTop:"30px"}} >
+            <MenuItem key="easy" value="eaasy">Easy</MenuItem>
+            <MenuItem key="medium" value="medium">Medium</MenuItem>
+            <MenuItem key="hard" value="hard" >Hard</MenuItem>
+          </TextField>
+        </div>
         <div className="banner">
           <img
             className="bannerImage"
