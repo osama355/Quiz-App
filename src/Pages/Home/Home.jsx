1833218f6b378c270402a9a39dd54d6d0e2d02b0
osama355 <muhammadosama3556@gmail.com>
2021-11-03 17:55:14 +0500

    update home page 1

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,36 +1,35 @@
 import React from "react";
 import "./home.css";
-import {TextField, MenuItem} from '@mui/material'
-import Categories, {} from '../../Data/Categories'
+import {TextField, MenuItem,Button} from '@mui/material'
+import Categories from '../../Data/Categories'
 
 function Home() {
   return (
-    <div>
-      <div className="heading">
-        <h3>Settings</h3>
-      </div>
+    <div className="home">
       <div className="main">
         <div className="form">
           <TextField className="textField1" style={{marginBottom:"30px"}} variant="outlined" label="Enter Name" />
           <TextField variant="outlined" className="textField2" select label="Select category">
             {
               Categories.map((val,key)=>(
-                <MenuItem key={key}>
+                <MenuItem key={key} value={key}>
                   {val.category}
                 </MenuItem>
               ))
             } 
           </TextField>
           <TextField select label="Select difficulty" variant="outlined" style={{marginTop:"30px"}} >
-            <MenuItem key="easy" value="eaasy">Easy</MenuItem>
+            <MenuItem key="easy" value="easy">Easy</MenuItem>
             <MenuItem key="medium" value="medium">Medium</MenuItem>
             <MenuItem key="hard" value="hard" >Hard</MenuItem>
           </TextField>
+          <Button variant="contained" className="button">Start Quiz</Button>
         </div>
+
         <div className="banner">
           <img
             className="bannerImage"
-            src="./Assets/banner.svg"
+            src="./Assets/banner2.svg"
             alt="bannerImage"
           />
         </div>
